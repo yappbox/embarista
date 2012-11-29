444e6b1d23bd1dcc382274a3461ed52e563db66d
Luke Melia <luke@lukemelia.com>
2012-11-29 02:08:54 -0500

    Use the manifest in the generate_index rake task

--- lib/embarista/dynamic_index.rb
+++ lib/embarista/dynamic_index.rb
@@ -6,15 +6,26 @@ module Embarista
     class Generator
       attr_reader :env_config, :yapp_env, :manifest_id, :yapp_config
 
-      def initialize(erb_path)
+      def initialize(erb_path, manifest_id)
         @erb_path = erb_path
         @env_config = Yapp.load_config
         @yapp_env = ENV.fetch('YAPP_ENV').to_sym
         @yapp_config = env_config.fetch(yapp_env)
-        @manifest_id = ENV.fetch('MANIFEST_ID')
+        @manifest_id = manifest_id
+        prepare_manifest
+      end
+
+      def prepare_manifest
+        if @manifest_id == 'dev'
+          @manifest = {}
+        else
+          #TODO: ensure the manifest is on s3
+          @manifest = YAML.load_file("tmp/public/#{@manifest_id}.yml")
+        end
       end
 
       def manifest_url(source)
+        source = @manifest[source] || source
         "//#{yapp_config.domains.assets_cdn}#{source}"
       end
 
@@ -31,7 +42,7 @@ module Embarista
         @app = app
         @erb_path = opts[:erb] || 'app/index.html.erb'
         @path_info = opts.fetch(:path_info)
-        @generator = Embarista::DynamicIndex::Generator.new(@erb_path)
+        @generator = Embarista::DynamicIndex::Generator.new(@erb_path, 'dev')
       end
 
       def call(env)
@@ -60,10 +71,11 @@ module Embarista
       end
 
       def define
-        task name do
+        task name, :manifest_id do |t, args|
+          manifest_id = args[:manifest_id]
           mkdir_p output_dir
           File.open("#{output_dir}/index.html", "w") do |f|
-            generator = Embarista::DynamicIndex::Generator.new(erb_path)
+            generator = Embarista::DynamicIndex::Generator.new(erb_path, manifest_id)
             f.write(generator.html)
           end
         end
