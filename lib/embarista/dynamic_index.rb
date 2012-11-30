aa940feb20dc7589243490af97505d631f9f3759
Kris Selden <kris.selden@gmail.com>
2012-11-29 20:20:55 -0800

    set current index task

--- lib/embarista/dynamic_index.rb
+++ lib/embarista/dynamic_index.rb
@@ -59,6 +59,64 @@ module Embarista
       end
     end
 
+    class SetCurrentTask < ::Rake::TaskLib
+      attr_accessor :name, :app
+
+      def initialize(name = :set_current_index)
+        @name = name
+
+        yield self if block_given?
+
+        raise 'app must be set' unless @app
+        define
+      end
+
+      def redis_url
+        ENV['REDISTOGO_URL'] ||= begin
+          yapp_env = ENV.fetch('YAPP_ENV')
+          redis_url = case yapp_env
+          when 'dev'
+            "redis://0.0.0.0:6379/"
+          when 'qa'
+            Bundler.with_clean_env do
+              `heroku config:get REDISTOGO_URL --app qa-yapp-cedar`.chomp
+            end
+          when 'prod'
+            Bundler.with_clean_env do
+              `heroku config:get REDISTOGO_URL --app yapp-cedar`.chomp
+            end
+          else
+            raise "don't know how to get redis connection for #{yapp_env}"
+          end
+        end
+      end
+
+      def redis
+        $redis ||= begin
+          require 'uri'
+          require 'redis'
+
+          uri = URI.parse(redis_url)
+
+          Redis.new(
+            :host => uri.host,
+            :port => uri.port,
+            :password => uri.password
+          )
+        end
+      end
+
+      def define
+        task name, :manifest_id do |t, args|
+          require 'redis'
+
+          manifest_id = args[:manifest_id]
+
+          redis.set("#{app}:index:current", manifest_id)
+        end
+      end
+    end
+
     class Task < ::Rake::TaskLib
       attr_accessor :name, :erb_path, :app
 
