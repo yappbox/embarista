818018a632b9be59f23dd3e2c5083cf340a6c9d7
Luke Melia <luke@lukemelia.com>
2012-12-10 13:49:38 -0500

    Add TaskLib for updating ember-data and jquery

--- lib/embarista/tasks/updater.rb
+++ lib/embarista/tasks/updater.rb
@@ -4,6 +4,14 @@ require 'rake/tasklib'
 module Embarista
   module Updater
 
+    def self.update_asset_file(regexp, replacement)
+      assetfile_contents = File.read('Assetfile')
+      assetfile_contents.gsub!(regexp, replacement)
+      File.open('Assetfile', 'w') do |f|
+        f.write(assetfile_contents)
+      end
+    end
+
     class UpdateEmberTask < ::Rake::TaskLib
       attr_accessor :name
 
@@ -40,6 +48,71 @@ module Embarista
             update_asset_file(old_sha, new_sha)
           end
           puts "Updated from #{old_sha} to #{new_sha}"
+        end
+      end
+    end
+
+    class UpdateEmberDataTask < ::Rake::TaskLib
+      attr_accessor :name
+
+      def initialize(name = :update_ember_data)
+        @name = name
+        yield self if block_given?
+        define
+      end
+
+      def define
+        task name do |t, args|
+          old_sha, new_sha = nil, nil
+          regexp = /ember-data-([0-9a-f]{40})/
+          app_vendor_path = File.expand_path("app/vendor")
+          FileUtils.cd(app_vendor_path) do
+            old_filename = Dir['*'].grep(regexp)[0]
+            old_filename =~ regexp
+            old_sha = $1
+          end
+          raise "Couldn't find current ember-data js version" if old_sha.nil?
+          FileUtils.cd('../ember-data') do
+            new_sha = `git rev-parse HEAD`.chomp
+            `bundle && bundle exec rake dist`
+            FileUtils.cd('./dist') do
+              FileUtils.cp('ember-data.js', "#{app_vendor_path}/ember-data-#{new_sha}.js")
+              FileUtils.cp('ember-data.min.js', "#{app_vendor_path}/ember-data-#{new_sha}.min.js")
+            end
+          end
+          if old_sha != new_sha
+            FileUtils.cd(app_vendor_path) do
+              FileUtils.rm("ember-data-#{old_sha}.js")
+              FileUtils.rm("ember-data-#{old_sha}.min.js")
+            end
+            update_asset_file(old_sha, new_sha)
+          end
+          puts "Updated from #{old_sha} to #{new_sha}"
+        end
+      end
+    end
+
+    class UpdateJqueryTask < ::Rake::TaskLib
+      attr_accessor :name
+
+      def initialize(name = :update_jquery)
+        @name = name
+        yield self if block_given?
+        define
+      end
+
+      def define
+        task name do |t, args|
+          version = ENV['VERSION']
+          raise "please supply VERSION env var to specify jQuery version" if version.nil?
+          cd('./app/vendor') do
+            # remove old jquerys
+            rm Dir['jquery-*.js']
+            sh "curl -O http://code.jquery.com/jquery-#{version}.js"
+            sh "curl -O http://code.jquery.com/jquery-#{version}.min.js"
+          end
+          update_asset_file(%r{JQUERY_VERSION = '\d+\.\d+\.\d+'}, "JQUERY_VERSION = '#{version}'")
+          puts "Updated to jQuery #{version}"
         end
       end
     end
