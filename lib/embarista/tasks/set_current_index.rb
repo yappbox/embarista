174d7229eb38290aefe508eb19a65b5b00b9d470
Luke Melia <luke@lukemelia.com>
2013-07-23 17:34:37 -0400

    Add support for setting redis keys besides "index". To be used for A/B testing different branches.

--- lib/embarista/tasks/set_current_index.rb
+++ lib/embarista/tasks/set_current_index.rb
@@ -3,10 +3,11 @@ require 'rake/tasklib'
 
 module Embarista
   class SetCurrentIndexTask < ::Rake::TaskLib
-    attr_accessor :name, :app, :redis
+    attr_accessor :name, :app, :key, :redis
 
-    def initialize(name = :set_current_index)
+    def initialize(name = :set_current_index, key = 'index')
       @name = name
+      @key = key
 
       yield self if block_given?
 
@@ -18,11 +19,11 @@ module Embarista
 
     private
     def define
-      set_current_task = task name, :manifest_id do |t, args|
+      set_current_task = task(name, :manifest_id) do |t, args|
         manifest_id = args[:manifest_id] || App.env.to_s
 
-        puts "redis.set('#{app}:index:current', '#{manifest_id}')"
-        redis.set("#{app}:index:current", manifest_id)
+        puts "redis.set('#{app}:#{key}:current', '#{manifest_id}')"
+        redis.set("#{app}:#{key}:current", manifest_id)
       end
       set_current_task.add_description "Activates a manifest in the given #{App.env_var}"
     end
